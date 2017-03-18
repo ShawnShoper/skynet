ff7f1e32155d218d969dfdf8927f9214c54c3494
ShawnShoper <xiehao3692@vip.qq.com>
2017-03-19 00:32:24 +0800

    抓取接口

--- crawler/crawler-common/src/main/java/org/skynet/crawler/common/Crawler.java
+++ crawler/crawler-common/src/main/java/org/skynet/crawler/common/Crawler.java
@@ -1,9 +1,21 @@
 package org.skynet.crawler.common;
 
+import java.util.Map;
+
 /**
  * Created by Shoper on 2017/3/19.
  * 爬取接口....
  */
 public interface Crawler {
+    /**
+     * 列表抓取,传入列表页url
+     * @param url
+     */
+    Map<String,Object> crawlerList(String url,Object... obj);
 
+    /**
+     * 单数据抓取
+     * @param url
+     */
+    Map<String,Object> crawler(String url,Object... obj);
 }
