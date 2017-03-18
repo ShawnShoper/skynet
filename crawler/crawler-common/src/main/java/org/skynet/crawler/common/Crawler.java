79178694be8599fa7591511077aea9fc5162d385
ShawnShoper <xiehao3692@vip.qq.com>
2017-03-19 00:20:14 +0800

    爬虫公共

---
+++ crawler/crawler-common/src/main/java/org/skynet/crawler/common/Crawler.java
@@ -0,0 +1,9 @@
+package org.skynet.crawler.common;
+
+/**
+ * Created by Shoper on 2017/3/19.
+ * 爬取接口....
+ */
+public interface Crawler {
+
+}
---
+++ crawler/crawler-mall/crawler-mall-jd/src/main/java/org/skynet/crawler/mall/jd/JDCrawler.java
@@ -0,0 +1,8 @@
+package org.skynet.crawler.mall.jd;
+
+/**
+ * Created by Shoper on 2017/3/19.
+ */
+public class JDCrawler {
+
+}
