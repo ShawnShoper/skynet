8e4d0d48124ecca5d770b6a61e06d78a8ae1952f
jason <939958749@qq.com>
2017-03-19 01:54:54 +0800

    add sample page of index

--- crawler/crawler-mall/crawler-mall-gome/src/test/java/org/skynet/crawler/mall/gome/fetcher/HttpClientFetcherTest.java
+++ crawler/crawler-mall/crawler-mall-gome/src/test/java/org/skynet/crawler/mall/gome/fetcher/HttpClientFetcherTest.java
@@ -1,9 +1,15 @@
 package org.skynet.crawler.mall.gome.fetcher;
 
 import com.alibaba.fastjson.JSON;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
 import org.skynet.crawler.mall.gome.source.InputSource;
 import org.skynet.crawler.mall.gome.source.SourceType;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Medusar on 19/03/2017.
@@ -31,8 +37,9 @@ public class HttpClientFetcherTest {
         System.out.println(JSON.toJSONString(pageContent));
     }
 
+
     @Test
-    public void testFetchNotExist(){
+    public void testFetchNotExist() {
         HttpClientFetcher fetcher = new HttpClientFetcher();
 
         InputSource source = new InputSource() {
@@ -50,6 +57,27 @@ public class HttpClientFetcherTest {
         PageContent pageContent = fetcher.fetch(source);
 
         System.out.println(JSON.toJSONString(pageContent));
+    }
+
+
+    @Test
+    public void testFetchSample() throws IOException {
+        InputSource source = new InputSource() {
+            @Override
+            public String url() {
+                return "http://www.gome.com.cn";
+            }
+
+            @Override
+            public SourceType type() {
+                return null;
+            }
+        };
+
+        PageContent pageContent = new HttpClientFetcher().fetch(source);
+
+        IOUtils.write(pageContent.getResponseBody(), new FileWriter("src/main/resources/samples/index."
+                + new SimpleDateFormat("yyyyMMddHHmmss").format(new Date()) + ".html"));
     }
 
 }
