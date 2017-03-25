28ad121361a45e511750fbda5333ec325ecda6c3
ShawnShoper <xiehao3692@vip.qq.com>
2017-03-25 22:50:51 +0800

    jd价格接口

--- crawler/crawler-mall/crawler-mall-jd/src/main/java/org/skynet/crawler/mall/jd/JDGoodsCrawler.java
+++ crawler/crawler-mall/crawler-mall-jd/src/main/java/org/skynet/crawler/mall/jd/JDGoodsCrawler.java
@@ -73,13 +73,4 @@ public class JDGoodsCrawler implements Crawler {
         }
         return data;
     }
-
-//    private double crawlerPrice(String skuid) throws IOException {
-//        Price target = SpringMvcFeign.target(Price.class, "https://p.3.cn/", 10, timeout);
-//        skuid = "J_" + skuid;
-//        String price = target.getPrice(skuid, "2_2815_51975_0", "1",System.currentTimeMillis());
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        double p = objectMapper.readTree(price).get(0).get("p").asDouble();
-//        return p;
-//    }
 }
